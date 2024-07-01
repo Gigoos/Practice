ff2cd04761706bfecbf7c146d24becc66571b036
Gigoos <gigoos2012@gmail.com>
2024-07-01 12:20:51 -0400

    Add function with nested if statement

--- 01.hello_world.py
+++ 01.hello_world.py
@@ -1,10 +1,15 @@
+
 hello = "world"
 world = "hello"
 
 print(f"{world} {hello} !!!!!!")
 
 def hello(value):
-    print(value)
+    if value == "hello" or value == "hey":
+        print("Hi!")
+    elif value == "yo":
+        print("Yo back!")
+    else:
+        print("Get outta here")
 
-
-hello("Yooooo!!!!")
+hello("yoo")
